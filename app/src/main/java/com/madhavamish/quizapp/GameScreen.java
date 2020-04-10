2eda6268b8132bf54ef5df77b8e555021d698e76
amadhav2021 <amadhav2021@gmail.com>
2020-04-10 13:28:16 -0400

    Lab6c: Tweaked the layout and updated lifeline buttons to only be used once.

--- app/src/main/java/com/madhavamish/quizapp/GameScreen.java
+++ app/src/main/java/com/madhavamish/quizapp/GameScreen.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameScreen extends AppCompatActivity {
 
-    Button final_answer;
-    TextView a, b, c, d;
+    Button final_answer, poll, eliminate, a, b, c, d;
+    boolean poll_used, eliminate_used = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +21,7 @@ public class GameScreen extends AppCompatActivity {
         b = findViewById(R.id.choiceB);
         c = findViewById(R.id.choiceC);
         d = findViewById(R.id.choiceD);
+
 
         final_answer = findViewById(R.id.final_answer);
         final_answer.setEnabled(false);
@@ -34,11 +35,48 @@ public class GameScreen extends AppCompatActivity {
                 d.setBackgroundColor(getResources().getColor(R.color.green));
             }
         });
+
+        poll = findViewById(R.id.poll);
+        poll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(poll_used){
+                    Toast.makeText(getApplicationContext(), "You have already used the audience poll lifeline", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    poll.setBackground(getResources().getDrawable(R.drawable.circle_gray, null));
+                    poll.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                    poll.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.lose_option_foreground, 0, 0);
+                    poll_used = true;
+                }
+            }
+        });
+
+        eliminate = findViewById(R.id.eliminate);
+        eliminate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(eliminate_used){
+                    Toast.makeText(getApplicationContext(), "You have already used the 50:50 lifeline", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    eliminate.setBackground(getResources().getDrawable(R.drawable.circle_gray, null));
+                    eliminate.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                    eliminate.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.lose_option_foreground, 0, 0);
+                    eliminate_used = true;
+                }
+            }
+        });
     }
 
     public void answer_selected(View view) {
+        a.setBackgroundColor(getResources().getColor(R.color.black));
+        b.setBackgroundColor(getResources().getColor(R.color.black));
+        c.setBackgroundColor(getResources().getColor(R.color.black));
+        d.setBackgroundColor(getResources().getColor(R.color.black));
         view.setBackgroundColor(getResources().getColor(R.color.yellow));
         final_answer.setEnabled(true);
+        final_answer.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
     }
 
 }
